d7066c49dc1b25eed1912f803f66b9b07ddbb58b
Mike Acton <macton@gmail.com>
2015-06-22 14:46:43 -0700

    remove use

--- s.js
+++ s.js
@@ -20,27 +20,10 @@ var cv = curry( function( x, y ) {
   return (typeof x === 'function') ? x(y) : x;
 });
 
-var use = function( pluginPath ) {
-  var name = path.extname( pluginPath ).substr(1);
-  var obj  = require( pluginPath );
-  var keys = Object.keys( obj );
-
-  Object.keys(obj).forEach( function( key ) {
-    if ( typeof obj[key] === 'function' ) {
-      if ( obj[key].length > 1 ) {
-        obj[key] = curry( obj[key] );
-      }
-    }
-  });
-
-  S[name] = obj;
-};
-
 var S = {
   curry    : curry,
   cv       : cv,
   identity : function( x ) { return x; },
-  use      : use,
 };
 
 exports = module.exports = S;
